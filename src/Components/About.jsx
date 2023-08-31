8560b78581f60af010c0ff8fb1cfa2830008b24e
RJRYT <robinjratr@gmail.com>
2023-08-31 10:55:36 +0530

    initial commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a passonate full stack developer from kerala.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "React",
+  "Nodejs",
+  "MongoDB",
+  "Express js",
 ];
 
 /**
